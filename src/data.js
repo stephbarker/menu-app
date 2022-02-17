9c4e1d73c7c2381379e7e7bb2b9795b17372c7c1
Steph Barker <sbarkerdrums@gmail.com>
2022-02-16 16:59:24 -0800

    adds data.js file for menu items

---
+++ src/data.js
@@ -0,0 +1,45 @@
+const menu = [
+    {
+        id: 1,
+        img: './img/apple.jpg',
+        title: "Crisp Apple",
+        desc: "An apple a day keeps the doctor away, so go ahead and get your daily dose in!",
+        price: 2.99,
+    },
+    {
+        id: 2,
+        img: './img/breakfast-bowl.jpg',
+        title: "Breakfast Bowl",
+        desc: "Healthy grains, topped with a perfectly poached egg.",
+        price: 16.99, 
+    },
+    {
+        id: 3,
+        img: './img/burger.jpg',
+        title: "Burger & Fries",
+        desc: "100% grass-fed beef burger, served with our famous truffle fries.",
+        price: 22.99,
+    },
+    {
+        id: 4,
+        img: './img/eggs.jpg',
+        title: "Farm Fresh Eggs",
+        desc: "Farm fresh eggs, available to take home by the dozen!.",
+        price: 7.99,
+    },
+    {
+        id: 5,
+        img: './img/fries.jpg',
+        title: "Truffle Fries",
+        desc: "Our famous truffle fries, yes please!",
+        price: 8.99,
+    },
+    {
+        id: 6,
+        img: './img/steak.jpg',
+        title: "Steak & Frites",
+        desc: "Our dry-aged NY Strip, cooked to perfection and served up with rosemary seasoned frites",
+        price: 38.99, 
+    },
+];
+export default menu;
