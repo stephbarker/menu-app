f28c3e321db861efeb94485979828f1f3ab9f59d
Steph Barker <sbarkerdrums@gmail.com>
2022-02-17 13:05:22 -0800

    adds nav bar and sets up function to add an item

--- src/App.js
+++ src/App.js
@@ -6,13 +6,22 @@ import items from './data';
 
 function App() {
   //Set State
-  const [menuItems] = useState(items);
+  const [menuItems,setMenuItems] = useState(items);
+
+  //Helper Functions - Add Item
+  const addItem = item => {
+    item.id = items.length + 1;
+    setMenuItems([...items, item]);
+  }
 
   return (
     <main>
       <section>
         <div className="title">
         <h2 className>My Menu</h2>
+        <nav>
+          <li>Add Item</li>
+        </nav>
         </div>
         <Menu items={menuItems}/>
       </section>
