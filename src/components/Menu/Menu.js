9155cd36171b39478dd0bd7c2cbbbe93c994bc34
Steph Barker <sbarkerdrums@gmail.com>
2022-02-16 18:13:48 -0800

    adds Menu component

---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Menu({ items }) {
+    return (
+        <div className="menu-container">
+            {items.map((item) => {
+                const { id, img, title, desc, price } = item;
+                return (
+                    <article key={id} className="menu-item">
+                        <img src={img} alt={title} className="image" />
+                        <div className="item-info">
+                            <header>
+                                <h4>{title}</h4>
+                                <h4 className="price">${price}</h4>
+                            </header>
+                            <p className="item-desc">{desc}</p>
+                        </div>
+                    </article>
+                );
+            })}   
+        </div>
+    );
+};
