6af86c5d50d44aa576d8a4575b25072349ddadbd
Steph Barker <sbarkerdrums@gmail.com>
2022-02-17 10:47:07 -0800

    adds helper functions for onChange and onSubmit

--- src/Forms/AddItem/AddItemForm.js
+++ src/Forms/AddItem/AddItemForm.js
@@ -5,17 +5,32 @@ export default function AddItemForm(props) {
     const initItem = {id: null, img: '', title: '', desc: '', price: ''}
     const [item, setItem] = useState(initItem);
 
+    //Helper Functions - onChange & onSubmit
+    const handleChange = e => {
+        const {name, value} = e.target;
+        setItem({...item, [name]: value});
+    };
+
+    const handleSubmit = e => {
+        //Prevent default page refresh
+        e.preventDefault();
+        //Check that all inputs are filled out
+        if(item.img && item.title && item.desc && item.price) {
+            handleChange(e, props.addItem(item));
+        };
+    };
+
     return (
        <form>
            <label for="img">Image Url:</label>
-           <input type="url" name="img" value={item.img}/>
+           <input type="url" name="img" value={item.img} onChange={handleChange}/>
            <label for="title">Title:</label>
-           <input type="text" name="title" value={item.title}/>
+           <input type="text" name="title" value={item.title} onChange={handleChange}/>
            <label for="desc">Description:</label>
-           <input type="text" name="desc" value={item.desc}/>
+           <input type="text" name="desc" value={item.desc} onChange={handleChange}/>
            <label for="price">Price:</label>
-           <input type="text" name="price" value={item.price}/>
-           <button className="submit-button" type="submit">Add Item</button>
+           <input type="text" name="price" value={item.price} onChange={handleChange}/>
+           <button className="submit-button" type="submit" onClick={handleSubmit}>Add Item</button>
        </form>
     );
 };
