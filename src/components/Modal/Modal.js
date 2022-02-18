b54a098ad27960ece2c13fac812fb88481cffa2d
Steph Barker <sbarkerdrums@gmail.com>
2022-02-18 15:43:50 -0800

    styles confirmation modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import './Modal.css';
 
 export default function Modal({ handleDeleteTrue, handleDeleteFalse }) {
     return (
-        <section>
-            <article>
+        <section className="modal">
+            <article className="modal-content">
                 <p>Are you sure?</p>
-                <div className="modal-buttons">
+                <div>
                 <button className="confirm-button" onClick={handleDeleteTrue}>Confirm</button>
                 <button className="cancel-button" onClick={handleDeleteFalse}>Cancel</button>
                 </div>
