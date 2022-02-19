1a37f5ef7653b921ad99ac20ec0b786f8395b2c5
Steph Barker <sbarkerdrums@gmail.com>
2022-02-19 10:45:03 -0800

    adds inline edit functionality using react-easy-edit

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,7 +1,18 @@
 import React from "react";
+import EasyEdit from 'react-easy-edit';
+import Swal from 'sweetalert2';
+
 import "./Menu.css";
 
 export default function Menu({ items, onDelete }) {
+
+    //Modal confirming save after Edit
+    const save = () => Swal.fire({
+        icon: 'success',
+        title: 'Yay...',
+        text: 'You have successfully edited this item!'
+      });
+
     return (
         <div className="menu-container">
             {items.map((item) => {
@@ -11,10 +22,10 @@ export default function Menu({ items, onDelete }) {
                         <img src={img} alt={title} className="image" />
                         <div className="item-info">
                             <header>
-                                <h4>{title}</h4>
-                                <h4 className="price">${price}</h4>
+                                <h4><EasyEdit type="text" onSave={save}  saveButtonLabel="Save" cancelButtonLabel="Cancel" placeholder={title} /></h4>
+                                <h4 className="price">$<EasyEdit type="text" onSave={save} saveButtonLabel="Save" cancelButtonLabel="Cancel" placeholder={price} /></h4>
                             </header>
-                            <p className="item-desc">{desc}</p>
+                            <p className="item-desc"><EasyEdit type="textarea" onSave={save} saveButtonLabel="Save" cancelButtonLabel="Cancel" placeholder={desc} /></p>
                             <button className="remove-button" onClick={() => onDelete(item.id)}>Remove Item</button>
                         </div>
                     </article>
